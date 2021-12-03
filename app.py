8fec3c0453909bec744b33633fc6994dd07bfb0d
nl000 <68584954+nl000@users.noreply.github.com>
2021-12-03 19:07:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 #Creates an application that is the name of our file i.e. app
 app = Flask(__name__)
 #configuring our application to connect to our postgres database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:09091990@localhost:5432/todoapp'
 #This creates an instance (called db) of our database that we can interact using SQLAlchemy
 db = SQLAlchemy(app)
 
@@ -23,19 +24,30 @@ db.create_all()
 #This controller retrieves data from the form when users click to submit/POST the data
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    #As the description is coming to the server as dictionary, we use the following code
-    description = request.form.get_json()['description']
-    #Using the Todo class defined above
-    todo = Todo(description=description)
-    #This adds the data to the pending-to-add into database list
-    db.session.add(todo)
-    #This commits the pending list i.e. makes the changes in the database
-    db.session.commit()
-    #jsonify will send JSON data back to the client
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        #As the description is coming to the server as dictionary, we use the following code
+        description = request.get_json()['description']
+        #Using the Todo class defined above
+        todo = Todo(description=description)
+        #This adds the data to the pending-to-add into database list
+        db.session.add(todo)
+        #This commits the pending list i.e. makes the changes in the database
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error=True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
 
+ 
 @app.route('/')
 #use render_template to specify the HTML file to render to the users whenever they visit this route.
 #MVC: the index() method is the Controller, index.html is the View and the data is the Model
